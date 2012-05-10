3998898992e4c57536122b32d6d510aa63be24be
Coen Jacobs <coenjacobs@gmail.com>
2012-05-10 23:44:16 +0200

    Added basic overview of coupons applied to cart

--- assets/woocommerce-debug-bar.js
+++ assets/woocommerce-debug-bar.js
@@ -15,4 +15,21 @@ jQuery( document ).ready( function( $ ) {
 		$('#wc-debug-bar-products').hide('fast');
 		return false;
 	});
+
+	$('#wc-debug-bar-show-coupons').show();
+	$('#wc-debug-bar-coupons').hide();
+
+	$('#wc-debug-bar-show-coupons').live('click', function() {
+		$('#wc-debug-bar-show-coupons').hide();
+		$('#wc-debug-bar-hide-coupons').show();
+		$('#wc-debug-bar-coupons').show('fast');
+		return false;
+	});
+
+	$('#wc-debug-bar-hide-coupons').live('click', function() {
+		$('#wc-debug-bar-hide-coupons').hide();
+		$('#wc-debug-bar-show-coupons').show();
+		$('#wc-debug-bar-coupons').hide('fast');
+		return false;
+	});
 });
--- classes/class-woocommerce-debug-bar-panel.php
+++ classes/class-woocommerce-debug-bar-panel.php
@@ -15,17 +15,17 @@ class WooCommerce_Debug_Bar_Panel extends Debug_Bar_Panel {
 
 		if ( isset( $woocommerce->cart ) ) {
 			echo '<p><strong>' . __( 'Number of products:', 'wc-debug-bar' ) . '</strong> ' . count( $woocommerce->cart->cart_contents ) . '<br />';
-			echo '<strong>' . __( 'Total quantity:', 'wc-debug-bar' ) . '</strong> ' . $woocommerce->cart->cart_contents_count . '<br />';
+			echo '<strong>' . __( 'Total quantity:', 'wc-debug-bar' ) . '</strong> ' . $woocommerce->cart->cart_contents_count . '</p>';
 
 			if ( ! empty( $woocommerce->cart->cart_contents ) ) {
-				echo '<a href="#" id="wc-debug-bar-hide-products">' . __( 'Hide cart products &uarr;', 'wc-debug-bar' ) .'</a>';
+				echo '<p><a href="#" id="wc-debug-bar-hide-products">' . __( 'Hide cart products &uarr;', 'wc-debug-bar' ) .'</a>';
 				echo '<a href="#" id="wc-debug-bar-show-products">' . __( 'Show cart products &darr;', 'wc-debug-bar' ) . '</a></p>';
 
 				echo '<div id="wc-debug-bar-products">';
 
 				echo '<h4>' . __( 'Products in cart', 'wc-debug-bar' ) . '</h4>';
 				
-				echo '<table id="wc-debug-bar-products-table">';
+				echo '<table class="wc-debug-bar-table" id="wc-debug-bar-products-table">';
 
 				echo '<tr>';
 					echo '<th>cart_contents_key</th>';
@@ -52,6 +52,29 @@ class WooCommerce_Debug_Bar_Panel extends Debug_Bar_Panel {
 				echo '</table>';
 
 				echo '</div>';
+			}
+
+			echo '<p><strong>' . __( 'Applied coupons:', 'wc-debug-bar' ) . '</strong> ' . count( $woocommerce->cart->applied_coupons ) . '</p>';
+
+			if ( ! empty( $woocommerce->cart->applied_coupons ) ) {
+				echo '<p><a href="#" id="wc-debug-bar-hide-coupons">' . __( 'Hide applied coupons &uarr;', 'wc-debug-bar' ) .'</a>';
+				echo '<a href="#" id="wc-debug-bar-show-coupons">' . __( 'Show applied coupons &darr;', 'wc-debug-bar' ) . '</a></p>';
+
+				echo '<div id="wc-debug-bar-coupons">';
+
+				echo '<h4>' . __( 'Coupons applied to cart', 'wc-debug-bar' ) . '</h4>';
+				
+				echo '<table class="wc-debug-bar-table" id="wc-debug-bar-coupons-table">';
+
+				echo '<tr>';
+					echo '<th>coupon_code</th>';
+				echo '</tr>';
+
+				foreach ( $woocommerce->cart->applied_coupons as $key => $value ) {
+					echo '<tr>';
+						echo '<td>' . $value . '</td>';
+					echo '</tr>';
+				}
 			}
 		} else {
 			echo '<p>' . __( 'Cart contents not available.', 'wc-debug-bar' ) .'</p>';
